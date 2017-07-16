ef54a92580ca0f8c40c9dec4869148882bd4d6f2
DarkMiata <samvermeulen@gmail.com>
2017-07-16 15:42:42 +0200

    Auto stash before revert of "delete contact - annotation route contactBundle"

--- src/DM/ContactBundle/Controller/DefaultController.php
+++ src/DM/ContactBundle/Controller/DefaultController.php
@@ -6,13 +6,22 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use DM\ContactBundle\Entity\Contact;
 use DM\ContactBundle\Form\ContactType;
 use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 class DefaultController extends Controller
   {
+  /**
+   * @Route("/contact/", name="dm_contact_homepage")
+   */
   public function indexAction() {
     return $this->render('DMContactBundle:Default:index.html.twig');
   }
   // ------------------------
+
+  /**
+  * @Route("/contact/add/", name="dm_contact_add")
+  */
   // page de formulaire d'ajout de contact
   public function addAction(Request $request) {
     $contact = new Contact;
@@ -35,7 +44,11 @@ class DefaultController extends Controller
     ));
   }
   // ------------------------
+  /**
+  *  @Route("/contact/view/{id}", name="dm_contact_view")
+  */
   // Afficage du contact n°id
+
   public function viewAction($id) {
     $em = $this->getDoctrine()->getManager();
 
@@ -52,17 +65,54 @@ class DefaultController extends Controller
     ));
   }
   // ------------------------
+  /**
+  *  @Route("/contact/list/{msg}", defaults={"msg" = ""}, name="dm_contact_list")
+  */
   // liste des contacts
-  public function listAction() {
+
+  public function listAction($msg="") {
     $em = $this->getDoctrine()->getManager();
 
     $contacts = $em->getRepository('DMContactBundle:Contact')->findAll();
 
-    return $this->render('DMContactBundle:Default:list.html.twig', array(
-          'contacts' => $contacts
+    return $this->render('DMContactBundle:Default:list.html.twig',
+        [ 'contacts'  => $contacts,
+          'msg'       => $msg,
+          ]);
+  }
+  // ------------------------
+  /**
+  *  @Route("/contact/delete/{id}", name="dm_contact_deleteconfirm")
+  */
+  public function deleteConfirmAction($id) {
+    $em = $this->getDoctrine()->getManager();
+
+    $contact = $em->getRepository('DMContactBundle:Contact')->findOneById($id);
+
+    return $this->render('DMContactBundle:Default:delete_confirm.html.twig', array(
+          'contact' => $contact
     ));
   }
   // ------------------------
+  /**
+   *  @Route("/contact/deletebyid/{id}", name="dm_contact_deletebyid")
+   */
+  // effacer un élément ID et redirection vers la page de liste.
 
+    public function deleteByIdAction($id) {
+    $em       = $this->getDoctrine()->getManager();
+    $contact  = $em->getRepository('DMContactBundle:Contact')->findOneById($id);
+    $em->remove($contact);
+    $em->flush();
+
+    return $this->redirectToRoute('dm_contact_list', array(
+      'msg' => "élément ".$id." bien effacé<br>"
+    ));
+
+  // ========================================
+  // ========================================
+
+
+  }
 // ========================================
   }
